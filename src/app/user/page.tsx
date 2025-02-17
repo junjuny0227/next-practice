245c97fd76ef863a20189e27c01adf228790c685
junjuny0227 <junjuny0227@gmail.com>
2025-02-18 01:19:46 +0900

    add User type

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,7 +1,37 @@
 import axios from "axios";
 
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
 const getData = async () => {
-  const { data } = await axios.get(
+  const { data } = await axios.get<User[]>(
     "https://jsonplaceholder.typicode.com/users"
   );
   return data;
