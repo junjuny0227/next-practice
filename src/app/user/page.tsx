ae0a745774eac81bb5ea7c96609da0be0975f59e
junjuny0227 <junjuny0227@gmail.com>
2025-02-18 01:18:29 +0900

    fetch data in server component

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,5 +1,31 @@
-const UserPage = () => {
-  return <h1>User Page</h1>;
+import axios from "axios";
+
+const getData = async () => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  return data;
+};
+
+const UserPage = async () => {
+  const users = await getData();
+
+  return (
+    <div>
+      <h1>User Page</h1>
+      <ul>
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              <h3>{user.name}</h3>
+              <p>Email: {user.email}</p>
+              <p>Phone: {user.phone}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default UserPage;
